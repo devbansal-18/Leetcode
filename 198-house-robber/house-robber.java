b19b7d0fbe7845a2ed52197dc2cd87e1085fbc1e
DEV BANSAL <bansaldev3010@gmail.com>
2026-08-09 17:33:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (88.57%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,14 +1,13 @@
 class Solution {
-    static int maxloot(int[] nums,int idx,int []dp) {
-        if(idx>=nums.length) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int steal=nums[idx]+maxloot(nums,idx+2,dp);
-        int skip=maxloot(nums,idx+1,dp);
-        return dp[idx]=Math.max(steal,skip);
-    }
     public int rob(int[] nums) {
-        int []dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return maxloot(nums,0,dp);
-    }
+     int n=nums.length;
+     int []dp=new int[n];
+     dp[0]=nums[0];
+     if(n>1) dp[1]=Math.max(nums[0],nums[1]);
+     for(int i=2;i<n;i++) {
+        dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+     }
+     if(n==0) return dp[0];
+    else  return dp[n-1];
+}
 }
