c23268efa9f8eef48f3cdd00b2993025f2186004
DEV BANSAL <bansaldev3010@gmail.com>
2026-08-19 19:04:13 +0530

    Time: 2 ms (41.87%) | Memory: 42.9 MB (59.81%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+          Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++) {
+            char ch=s.charAt(i);
+            if(ch=='(') st.push(ch);
+            else {
+                if(st.size()==0 || st.peek()==')') st.push(ch);
+                else {
+                    if(st.peek()=='(') st.pop();
+                }
+            }
+        }
+        return st.size();
+    }
+}
