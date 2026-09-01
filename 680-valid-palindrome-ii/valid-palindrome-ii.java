985feb224796a31e428eefa14b07729120d24ecf
DEV BANSAL <bansaldev3010@gmail.com>
2026-09-01 18:21:16 +0530

    Time: 4 ms (99.07%) | Memory: 47.7 MB (68.89%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean fun(String s,int i,int j) {
+        while(i<j) {
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+       int l=0;
+       int r=s.length()-1;
+       while(l<r) {
+        if(s.charAt(l)!=s.charAt(r)) {
+            return fun(s,l+1,r) || fun(s,l,r-1);
+        }
+        l++;
+        r--;
+       }
+       return true;
+    }
+}
