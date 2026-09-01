5261f99efa06609c62ac96c830e2db1241ad678b
DEV BANSAL <bansaldev3010@gmail.com>
2026-09-01 19:42:19 +0530

    Time: 15 ms (59.79%) | Memory: 43.5 MB (69.68%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -14,7 +14,7 @@ class Solution {
         int len2=fun(s,i,i+1);
         int len=Math.max(len1,len2);
 
-        if(len>(end-start)) {
+        if(len>(end-start+1)) {
             start=i-(len-1)/2;
             end=i+len/2;
         }    
